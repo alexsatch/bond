d75ec2f2db5264e326e08b72e8f82f084a2a6349
Alex Chebaturkin <chebaturkin@gmail.com>
2016-04-30 04:22:27 -0700

    [WIP] Cloners work!

--- cs/src/precompile/Cloner.Builder.cs
+++ cs/src/precompile/Cloner.Builder.cs
@@ -8,30 +8,66 @@
     using System.Reflection.Emit;
     using Bond.Expressions;
 
-    public class PrecompiledClonerBuilder
+    public static class PrecompiledClonerBuilder
     {
-        private readonly TypeBuilder typeBuilder;
-
-        public PrecompiledClonerBuilder(TypeBuilder typeBuilder)
-        {
-            this.typeBuilder = typeBuilder;
-        }
-
-        public IEnumerable<Expression> Build<T>(Func<Expression, Expression, Expression> deferredDeserialize)
+        internal static IEnumerable<Expression> Build<T>(Func<Expression, Expression, Expression> deferredDeserialize)
         {
             return Cloner<T>.Generate(typeof(T), new DeserializerTransform<object>(deferredDeserialize, false), null);
         }
 
-        public MethodBuilder[] Precompile(Expression[] expressions)
+        public static TypeBuilder PrecompileCloner<TSource>(ModuleBuilder moduleBuilder)
+        {
+            var facadeBuilder = moduleBuilder.DefineType("PrecompiledBond." + typeof(TSource).Name + "ClonerHelper", TypeAttributes.NotPublic | TypeAttributes.Class | TypeAttributes.Sealed | TypeAttributes.Abstract);
+            var fieldBuilder = facadeBuilder.DefineField("CloneOp", typeof(Func<object, object>[]), FieldAttributes.Static | FieldAttributes.Assembly | FieldAttributes.InitOnly);
+            var facadeType = facadeBuilder.CreateType();
+            var fieldInfo = facadeType.GetField("CloneOp", BindingFlags.Static | BindingFlags.NonPublic);
+
+            var typeBuilder = moduleBuilder.DefineType("PrecompiledBond." + typeof(TSource).Name + "Cloner", TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.Sealed | TypeAttributes.Abstract);
+            
+            Func<Expression, Expression, Expression> deferredDeserialize = (o, i) =>
+            {
+                var arrayIndex = Expression.ArrayIndex(Expression.Field(null, fieldInfo), i);
+                return Expression.Invoke(arrayIndex, o);
+            };
+
+            var expressions = Build<TSource>(deferredDeserialize);
+            var methods = BuildMethods(typeBuilder, expressions.ToArray()).ToArray();
+
+            var staticCtor = typeBuilder.DefineConstructor(MethodAttributes.Private | MethodAttributes.Static, CallingConventions.Standard, Type.EmptyTypes);
+            var generator = staticCtor.GetILGenerator();
+
+            generator.Emit(OpCodes.Ldc_I4, methods.Length);
+            generator.Emit(OpCodes.Newarr, typeof(Func<object, object>));
+            generator.Emit(OpCodes.Stsfld, fieldInfo);
+
+            for (int k = 0; k < methods.Length; k++)
+            {
+                generator.Emit(OpCodes.Ldsfld, fieldInfo);
+                generator.Emit(OpCodes.Ldc_I4, k);
+                generator.Emit(OpCodes.Ldnull);
+                generator.Emit(OpCodes.Ldftn, methods[k]);
+                generator.Emit(OpCodes.Newobj, typeof(Func<object, object>).GetConstructor(new[] { typeof(object), typeof(IntPtr) }));
+                generator.Emit(OpCodes.Stelem_Ref);
+            }
+
+            generator.Emit(OpCodes.Ret);
+
+            return typeBuilder;
+        }
+
+        private static MethodBuilder[] BuildMethods(TypeBuilder typeBuilder, Expression[] expressions)
         {
             var result = new MethodBuilder[expressions.Length];
             for (int i = 0; i < expressions.Length; i++)
             {
                 var expression = expressions[i];
                 var src = Expression.Parameter(typeof(object), "source");
-                Expression<Func<object, object>> body = Expression.Lambda<Func<object, object>>(expression, src);
+                Expression<Func<object, object>> body = (Expression<Func<object, object>>) expression;
 
-                result[i] = typeBuilder.DefineMethod("Clone_" + i, MethodAttributes.Private | MethodAttributes.Static, typeof(object), new[] {typeof(object)});
+                if (i == 0)
+                    result[0] = typeBuilder.DefineMethod("Clone", MethodAttributes.Public | MethodAttributes.Static, typeof(object), new[] {typeof(object)});
+                else
+                    result[i] = typeBuilder.DefineMethod("Clone" + i, MethodAttributes.Private | MethodAttributes.Static, typeof(object), new[] {typeof(object)});
 
                 body.CompileToMethod(result[i]);
             }
--- cs/test/precompile/PrecompiledTests.cs
+++ cs/test/precompile/PrecompiledTests.cs
@@ -17,64 +17,10 @@ namespace UnitTest
             var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("DataSchemas.PrecompiledBond"), AssemblyBuilderAccess.RunAndSave);
             var moduleBuilder = assemblyBuilder.DefineDynamicModule("DataSchemas.PrecompiledBond.dll");
 
-//            PrecompileCloner<BasicTypes>(moduleBuilder).CreateType();
-            PrecompileCloner<FieldOfStructWithAliases>(moduleBuilder).CreateType();
+            PrecompiledClonerBuilder.PrecompileCloner<FieldOfStructWithAliases>(moduleBuilder).CreateType();
 
             assemblyBuilder.Save("DataSchemas.PrecompiledBond.dll");
         }
 
-        private static TypeBuilder PrecompileCloner<TSource>(ModuleBuilder moduleBuilder)
-        {
-            var facadeBuilder = moduleBuilder.DefineType("PrecompiledBond." + typeof(TSource).Name + "ClonerHelper", TypeAttributes.NotPublic | TypeAttributes.Class);
-            var fieldBuilder = facadeBuilder.DefineField("CloneOp", typeof(Func<object, object>[]), FieldAttributes.Static | FieldAttributes.Assembly | FieldAttributes.InitOnly);
-            var facadeType = facadeBuilder.CreateType();
-            var fieldInfo = facadeType.GetField("CloneOp", BindingFlags.Static | BindingFlags.NonPublic);
-
-            var typeBuilder = moduleBuilder.DefineType("PrecompiledBond." + typeof(TSource).Name + "Cloner", TypeAttributes.Public | TypeAttributes.Class);
-            var builder = new PrecompiledClonerBuilder(typeBuilder);
-            Func<Expression, Expression, Expression> deferredDeserialize = (o, i) =>
-            {
-                var arrayIndex = Expression.ArrayIndex(Expression.Field(null, fieldInfo), i);
-                return Expression.Invoke(arrayIndex, o);
-            };
-
-            var expressions = builder.Build<TSource>(deferredDeserialize);
-            var methods = builder.Precompile(expressions.ToArray()).ToArray();
-
-            var staticCtor = typeBuilder.DefineConstructor(MethodAttributes.Private | MethodAttributes.Static, CallingConventions.Standard, Type.EmptyTypes);
-            var generator = staticCtor.GetILGenerator();
-
-            generator.Emit(OpCodes.Ldc_I4, methods.Length);
-            generator.Emit(OpCodes.Newarr, typeof(Func<object, object>));
-            generator.Emit(OpCodes.Stsfld, fieldInfo);
-
-            for (int k = 0; k < methods.Length; k++)
-            {
-                generator.Emit(OpCodes.Ldsfld, fieldInfo);
-                generator.Emit(OpCodes.Ldc_I4, k);
-                generator.Emit(OpCodes.Ldnull);
-                generator.Emit(OpCodes.Ldftn, methods[k]);
-                generator.Emit(OpCodes.Newobj, typeof(Func<object, object>).GetConstructor(new [] { typeof(object), typeof(IntPtr)}));
-                generator.Emit(OpCodes.Stelem_Ref);
-            }
-            
-            generator.Emit(OpCodes.Ret);
-
-            return typeBuilder;
-        }
-
-        private static Func<object, object>[] clone;
-
-        private static object Clone_X(object o)
-        {
-            return o;
-        }
-
-        public static void M()
-        {
-            clone = new Func<object, object>[2];
-            clone[0] = Clone_X;
-            clone[1] = Clone_X;
-        }
     }
 }
